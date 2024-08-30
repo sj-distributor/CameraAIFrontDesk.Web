e2178c0c8ba2cc9f54b6a4a7900222dcae10586b
Mindy.L <mindy.li@sjfood.com>
2024-08-30 14:01:43 +0800

    enhance

--- web/src/utils/error-message.ts
+++ web/src/utils/error-message.ts
@@ -1,24 +1,6 @@
 import i18n from "i18next";
 import KEYS from "@/i18n/keys/error-message";
 
-const { t } = i18n;
-
-const ERROR_MESSAGES = {
-  DUPLICATE_CAMERA: t(KEYS.DUPLICATE_CAMERA, { ns: "errorMessage" }),
-  DATABASE_ERROR: t(KEYS.DATABASE_ERROR, { ns: "errorMessage" }),
-  INSUFFICIENT_RESOURCES: t(KEYS.INSUFFICIENT_RESOURCES, {
-    ns: "errorMessage",
-  }),
-  CAMERA_INFO_ERROR: t(KEYS.CAMERA_INFO_ERROR, { ns: "errorMessage" }),
-  CAMERA_CONNECTION_FAILED: t(KEYS.CAMERA_CONNECTION_FAILED, {
-    ns: "errorMessage",
-  }),
-  VIDEO_STREAM_ERROR: t(KEYS.VIDEO_STREAM_ERROR, { ns: "errorMessage" }),
-  TASK_PARAMETER_ERROR: t(KEYS.TASK_PARAMETER_ERROR, { ns: "errorMessage" }),
-  REPLAY_STREAM_ERROR: t(KEYS.REPLAY_STREAM_ERROR, { ns: "errorMessage" }),
-  PROGRAM_ERROR: t(KEYS.PROGRAM_ERROR, { ns: "errorMessage" }),
-};
-
 const ERROR_KEYWORDS = {
   DUPLICATE_CAMERA: "Reduplicative camera",
   DATABASE_ERROR: "gateway database error",
@@ -32,6 +14,24 @@ const ERROR_KEYWORDS = {
 };
 
 export const getErrorMessage = (error: string) => {
+  const { t } = i18n;
+
+  const ERROR_MESSAGES = {
+    DUPLICATE_CAMERA: t(KEYS.DUPLICATE_CAMERA, { ns: "errorMessage" }),
+    DATABASE_ERROR: t(KEYS.DATABASE_ERROR, { ns: "errorMessage" }),
+    INSUFFICIENT_RESOURCES: t(KEYS.INSUFFICIENT_RESOURCES, {
+      ns: "errorMessage",
+    }),
+    CAMERA_INFO_ERROR: t(KEYS.CAMERA_INFO_ERROR, { ns: "errorMessage" }),
+    CAMERA_CONNECTION_FAILED: t(KEYS.CAMERA_CONNECTION_FAILED, {
+      ns: "errorMessage",
+    }),
+    VIDEO_STREAM_ERROR: t(KEYS.VIDEO_STREAM_ERROR, { ns: "errorMessage" }),
+    TASK_PARAMETER_ERROR: t(KEYS.TASK_PARAMETER_ERROR, { ns: "errorMessage" }),
+    REPLAY_STREAM_ERROR: t(KEYS.REPLAY_STREAM_ERROR, { ns: "errorMessage" }),
+    PROGRAM_ERROR: t(KEYS.PROGRAM_ERROR, { ns: "errorMessage" }),
+  };
+
   for (const [key, keyword] of Object.entries(ERROR_KEYWORDS)) {
     if (error.includes(keyword)) {
       return ERROR_MESSAGES[key as keyof typeof ERROR_MESSAGES];
